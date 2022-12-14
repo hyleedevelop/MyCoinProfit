73409d620a46091f9784db5aaaa0c9446e61944e
Hoyeon Lee <hyleedevelop@gmail.com>
2022-12-14 23:54:02 +0900

    Make basic structure

---
+++ DCA Planner/View/PlanView.swift
@@ -0,0 +1,20 @@
+//
+//  PlanView.swift
+//  DCA Planner
+//
+//  Created by Eric on 2022/12/14.
+//
+
+import UIKit
+
+class PlanView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
